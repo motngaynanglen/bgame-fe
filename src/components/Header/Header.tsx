57204724b7932506b7ae477523c20c6803e547c0
VoPhucTin <86974800+VoPhucTin@users.noreply.github.com>
2024-11-27 11:40:10 +0700

    footer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,7 @@
-import { Menu } from "antd";
-import React from "react";
+"use client";
 import type { MenuProps } from "antd";
-import { SearchProps } from "antd/es/input";
-import Search from "antd/es/input/Search";
-import { AiOutlineShopping } from "react-icons/ai";
-import { BsPersonCircle } from "react-icons/bs";
-import { BsBag } from "react-icons/bs";
-import { BsSearch } from "react-icons/bs";
-
+import { Menu } from "antd";
+import { BsBag, BsPersonCircle, BsSearch } from "react-icons/bs";
 
 type MenuItem = Required<MenuProps>["items"][number];
 
@@ -59,8 +53,8 @@ const items: MenuItem[] = [
 export default function Header() {
   return (
     <div>
-      <div className="bg-white h-auto flex justify-between items-center px-10 py-2">
-        <div className="text-4xl text-green-700 basis-2 font-bold">
+      <div className="bg-white  flex justify-between items-center px-10 py-2">
+        <div className="text-3xl text-green-700 basis-2 font-bold">
           BoGemStore
         </div>
 
@@ -71,27 +65,27 @@ export default function Header() {
           // style={{ width: 200, borderColor: "black"}}
         /> */}
 
-        <form className=" basis-1/3">
-          <label
+        <form className="basis-1/3 h-auto">
+          {/* <label
             htmlFor="default-search"
-            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            className="mb-1 text-sm font-normal text-gray-900 sr-only dark:text-white"
           >
             Search
-          </label>
+          </label> */}
           <div className="relative">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <BsSearch className="fill-green-700"/>
+              <BsSearch className="fill-green-700" />
             </div>
             <input
               type="search"
               id="default-search"
-              className="block  w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className=" w-full p-3 ps-10 text-sm text-gray-900  border-gray-300 rounded-md bg-gray-50  "
               placeholder="Nhập tên board game..."
               required
             />
             <button
               type="submit"
-              className="text-white absolute end-2.5 bottom-2.5 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2  dark:focus:ring-blue-800"
+              className="text-white absolute end-2.5 bottom-2 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1  dark:focus:ring-blue-800"
             >
               Tìm Kiếm
             </button>
@@ -99,12 +93,15 @@ export default function Header() {
         </form>
 
         <div className="flex justify-items-center basis-4 space-x-10">
-          <BsBag className="size-10 fill-green-700" />
-          <BsPersonCircle className="size-10 fill-green-700" />
+          <BsBag className="size-7 fill-green-700" />
+          <BsPersonCircle
+            className="size-7
+           fill-green-700"
+          />
         </div>
       </div>
       <Menu
-        className="flex-1 justify-center"
+        className="flex justify-center space-x-48  uppercase  "
         theme="dark"
         mode="horizontal"
         defaultSelectedKeys={["2"]}
