133d93c845727459862032dfffd952af6238aa95
bachdoan-sa <bach.doansy@gmail.com>
2025-01-17 12:06:34 +0700

    Update page.tsx
    change to client side render

--- src/app/admin/content/aboutus/page.tsx
+++ src/app/admin/content/aboutus/page.tsx
@@ -1,15 +1,26 @@
-// import fs from 'fs';
-// import path from 'path';
-
-// const filePath = path.join(process.cwd(),"src", 'data', "content", 'aboutus.json');
+"use client"
 import contentApiRequest from "@/src/apiRequests/content";
+import { useEffect, useState } from "react";
 
-export default async function AboutUs() {
-    const data = await contentApiRequest.getAboutUs();
-    // const fileContent = fs.readFileSync(filePath, 'utf8');
+export default function AboutUs() {
+    const [content, setContent] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await contentApiRequest.getAboutUs();
+        setContent(response.data);
+      } catch (error) {
+        console.error('Error fetching content', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+    
     return (
         <>
-            {JSON.stringify(data)}
+            {JSON.stringify(content)}
         </>
     );
 }
