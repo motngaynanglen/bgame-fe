72bf47fd8b92c7d56b20321f7d48ea68a1755989
bachdoan-sa <bach.doansy@gmail.com>
2025-08-20 18:47:38 +0700

    refactor name and import

--- src/components/About/AboutRental.tsx
+++ src/components/About/AboutRental.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import React from "react";
-import StarBorder from "../Bits/StarBorder ";
+import StarBorder from "../Bits/StarBorder";
 
 export default function AboutRental() {
   const router = useRouter();
